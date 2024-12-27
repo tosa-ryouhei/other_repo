c4efe4b90428702034e195b2bd7b2053a178b099
tosa-ryouhei <tosa-ryouhei@optage.co.jp>
2024-12-27 19:51:54 +0900

    Update other.tf

--- other.tf
+++ other.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "terraform-import" {
 ## import ec2
 import{
  id="i-058469f6b8935cb09"
- to=aws_instance.terraform-import-ec2
+ to=aws_instance.terraform-import-instance
 }
 
 ## terraform import ec2
